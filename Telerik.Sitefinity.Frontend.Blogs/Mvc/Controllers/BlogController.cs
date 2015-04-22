4c92ddef96b1974fd78d12e8729ca251f3c90c37
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-04-22 18:25:00 +0300

    Added DetailsPageId

--- Telerik.Sitefinity.Frontend.Blogs/Mvc/Controllers/BlogController.cs
+++ Telerik.Sitefinity.Frontend.Blogs/Mvc/Controllers/BlogController.cs
@@ -98,6 +98,14 @@ namespace Telerik.Sitefinity.Frontend.Blogs.Mvc.Controllers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the id of the page where will be displayed details view for selected item.
+        /// </summary>
+        /// <value>
+        /// The details page id.
+        /// </value>
+        public Guid DetailsPageId { get; set; }
+
         #endregion
 
         #region Actions
@@ -151,6 +159,8 @@ namespace Telerik.Sitefinity.Frontend.Blogs.Mvc.Controllers
             this.ViewBag.CurrentPageUrl = SystemManager.CurrentHttpContext != null ? this.GetCurrentPageUrl() : string.Empty;
             this.ViewBag.RedirectPageUrlTemplate = this.ViewBag.CurrentPageUrl + redirectPageUrl;
             this.ViewBag.ItemsPerPage = this.Model.ItemsPerPage;
+            this.ViewBag.DetailsPageId = this.DetailsPageId;
+            this.ViewBag.OpenInSamePage = this.OpenInSamePage;
         }
 
         #endregion
