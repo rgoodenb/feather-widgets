fc2f3156307d7b8340abde3b013c2569d64eefe8
dzhenko <dzhenko@gmail.com>
2015-05-04 16:36:24 +0300

    Logged in/out form view changed

--- Telerik.Sitefinity.Frontend.Comments/Mvc/Scritps/Comments/comments.js
+++ Telerik.Sitefinity.Frontend.Comments/Mvc/Scritps/Comments/comments.js
@@ -3,6 +3,9 @@
         var template = $($('[data-sf-role="single-comment-template"]').html());
         var commentsContainer = $('[data-sf-role="comments-holder"]');
 
+        // Initially hide new comment form
+        var newCommentForm = $('[data-sf-role="comments-new-holder"]').hide();
+
         var sampleResponse = {
             Items: [
                 { Name: 'Dustin Burke', Date: 'May 14, 2013', Text: 'Wow!' },
@@ -10,10 +13,12 @@
                 { Name: 'Mark Gjerstad', Date: 'May 14, 2013', Text: 'I think Jobs\' legacy is that he innovated business models and through that disrupted different industries' },
                 { Name: 'Scott M. Fulton', Date: 'May 14, 2013', Text: 'Mr. Ross said the administration should make it clear that it has red lines that if crossed would result in a cutoff of aid. And he said it should enlist Saudi Arabia and the Persian Gulf emirates, which are even larger financial supporters of Egypt than the United States, to pressure the generals. “They should use their influence with the Egyptian military to make it easier for us,” he said. The debate over aid, some analysts said, underscored the lack of a fresh strategy for dealing with Egypt. The White House has not conducted a strategic review of its Egypt policy since the ouster of Hosni Mubarak in 2011, they pointed out.  So far, Mr. Kurtzer said, there has been no tension between Israel and the United States over Egypt because neither' }
             ],
-            Count: 4
+            Count: 2
         };
 
-        var sampleUserLoggedInResponse = false;
+        var sampleUserLoggedInResponse = {
+            IsLoggedIn: false
+        };
 
         var createComment = function (comment) {
             var newComment = template.clone(true);
@@ -24,7 +29,7 @@
 
             commentsContainer.append(newComment);
         };
-        
+
         var makeServiceCall = function () {
             return {
                 then: function (cb) {
@@ -43,19 +48,23 @@
 
         var loadComments = function () {
             makeServiceCall().then(function (response) {
-                response.Items.forEach(createComment);
-                $('[data-sf-role="comments-count"]').text(response.Count);
+                if (response.Count > 0) {
+                    $('[data-sf-role="comments-header"]').text(response.Count + $('[data-sf-role="comments-header-text"]').val());
+                    response.Items.forEach(createComment);
+                }
+                else {
+                    $('[data-sf-role="comments-header"]').text($('[data-sf-role="comments-form"]').hide().text());
+                    newCommentForm.show();
+                }
             });
         };
 
         $('[data-sf-role="comments-load-more"]').click(loadComments);
-        
+
         // Initial loading of comments
         loadComments();
 
         // New comment
-
-        var newCommentForm = $('[data-sf-role="comments-new-holder"]').hide();
 
         $('[data-sf-role="comments-form"]').click(function () {
             newCommentForm.toggle('slow');
@@ -70,10 +79,8 @@
         });
 
         makeIsUserLoggedInServiceCall().then(function (response) {
-            if (response === true) {
-                $('[data-sf-role="comments-new-name"]').remove();
-                $('[data-sf-role="comments-new-email"]').remove();
-                $('[data-sf-role="comments-new-website"]').remove();
+            if (response.IsLoggedIn) {
+                $('[data-sf-role="comments-new-logged-out-view"]').remove();
             }
         });
     });
